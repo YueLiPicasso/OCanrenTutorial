b05dca47dcf188b8e29e9690fe29e46fa9b55cb5
YueLiPicasso <yue.li.picasso@qq.com>
2020-10-24 15:41:54 +0800

    constant contr

---
+++ DefineTypes/weekdays/README.md
@@ -0,0 +1,9 @@
+## Weekdays
+
+In this lesson, we learn about working with variant types defined solely with
+constant constructors. Our example program is [weekdays.ml](weekdays.ml), where
+we define a type called _weekdays_ whose inhabitants are five distinct constant constructrs:
+_Monday_, _Tuesday_ etc. The relational programming that we want to perform on this type
+is relatively simple: we define the _next day_ relation. The emphasis, however, is on the
+extra infrastructure tht we must build in order to work with such basic variant values in OCanren.
+
---
+++ DefineTypes/weekdays/weekdays.ml
@@ -0,0 +1,29 @@
+open OCanren;;
+
+@type weekdays = Monday | Tuesday | Wednesday | Thursday | Friday with show;;
+
+module Inj = struct
+  let monday = fun () -> !!(Monday)
+  and tuesday = fun () -> !!(Tuesday)
+  and wednesday = fun () -> !!(Wednesday)
+  and thursday = fun () -> !!(Thursday)
+  and friday = fun () -> !!(Friday);;
+end;;
+
+open Inj;;
+
+let next = fun d1 d2 ->
+  ocanren{
+      d1 == Monday & d2 == Tuesday
+    | d1 == Tuesday & d2 == Wednesday
+    | d1 == Wednesday & d2 == Thursday
+    | d1 == Thursday & d2 == Friday
+    | d1 == Friday & d2 == Monday 
+    };;
+
+@type wdpairs = weekdays * weekdays with show;;
+
+let _ =
+  List.iter (fun x -> print_string (GT.show(wdpairs) x ^ "\n")) @@ 
+    Stream.take @@
+      run qr (fun q r ->  next q r) (fun q r -> project q, project r);;
