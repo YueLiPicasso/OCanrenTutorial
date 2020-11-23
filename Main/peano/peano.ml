de1aaaf8986f34c7c5c9f915221bda846e43f235
YueLiPicasso <yue.li.picasso@qq.com>
2020-11-23 14:41:20 +0800

    pa_ocanren elaboration

--- Main/peano/peano.ml
+++ Main/peano/peano.ml
@@ -1,5 +1,7 @@
 open OCanren;;
 
+(* types and auxiliaries *)
+
 module Ty = struct
   @type 'a t = O | S of 'a with show, gmap;;
   let fmap = fun f d -> GT.gmap(t) f d;;
@@ -18,10 +20,14 @@ let s = fun n  -> inj @@ F.distrib (S n);;
 
 let rec reify = fun env n -> F.reify reify env n;;
 
+(* utilities *)
+
 let rec lt a b =
   ocanren{ fresh n in b == S n &
                         { a == O
                         | fresh n' in a == S n' & lt n' n }};;
+
+let lte a b = ocanren{ a == b | lt a b };;
 
 let rec add a b c =
   ocanren{ a == O & b == c
@@ -35,4 +41,16 @@ let rec div a b q r =
              |  a == b  & q == S O & r == O
              |  lt b a  & fresh c, q' in add b c a & add (S O) q' q & div c b q' r }};;
 
+(* Euclidean algorithm *)
+let rec gcd a b c =
+      ocanren { { lte b a & fresh q in div a b q O & c == b }
+              | lt b a & fresh q, n in div a b q (S n) & gcd b (S n) c };;
 
+let lcm a b c = ocanren { fresh ab, g in div ab a b O & gcd a b g & div ab c g O };;
+
+let simplify a b a' b'= 
+      ocanren {  fresh n in b == S n &
+      {  a == O  & a' == O & b' == S O
+      |  fresh m in a == S m
+                    & fresh cm in div a cm a' O
+                                  & div b cm b' 0 & gcd a b cm } };;
--- Main/peano/peano.mli
+++ Main/peano/peano.mli
@@ -15,7 +15,21 @@ val s : groundi -> groundi;;
 val reify : VarEnv.t -> groundi -> logic;;
 
 val lt : groundi -> groundi -> goal;;
+(** [lt a b] : a is less than b *)
+
+val lte : groundi -> groundi -> goal;;
+(** [lte a b] : a is less than or equal to b *)
 
 val add : groundi -> groundi -> groundi -> goal;;
+(** [add a b c] : a plus b equals c *)
 
 val div : groundi -> groundi -> groundi -> groundi -> goal;;
+(** [div a b q r] : dividing a by b gives quotient q and remainder r *)
+
+val gcd : groundi -> groundi -> groundi -> goal;;
+(** [gcd a b c] : if the greatest common divisor of a and b is c, where a is no less than  b *)
+
+val lcm : groundi -> groundi -> groundi -> goal;;
+(** [lcm a b c] : if the least common multiple of a and b is c, where a is no less than  b *)
+
+
