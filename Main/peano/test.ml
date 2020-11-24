799e07fa9d9a74359761d2824d5757cec724b1e3
YueLiPicasso <yue.li.picasso@qq.com>
2020-11-24 12:18:35 +0800

    peano

--- Main/peano/test.ml
+++ Main/peano/test.ml
@@ -36,6 +36,7 @@ let _ =
 printf "\n###### test lte ######\n\n";;
    
 printf "\n Checking \n\n";;
+
 let _ = List.iter (fun x -> print_endline @@ GT.show(logic) x) @@ take @@
           run one
             (fun q -> ocanren { lte (S O) (S(S O)) & lte (S O) (S O) })
@@ -65,4 +66,91 @@ let _ =
   in List.iter printer
      @@ take ~n:10
      @@ run two (fun q r -> ocanren { lte q r }) (fun q r -> q#reify reify, r#reify reify);;
+
+printf "\n###### test add ######\n\n";;
+   
+printf "\n Checking \n\n";;
+
+let _ = List.iter (fun x -> print_endline @@ GT.show(logic) x) @@ take @@
+          run one
+            (fun q -> ocanren { add (S O) (S(S O)) (S(S(S O)))
+                                & add (S O) (S O) (S(S O))})
+            (fun q -> q#reify reify);;
+
+let _ = Printf.printf "%d answer(s) found.\n" @@ List.length @@ take @@
+          run one
+            (fun q -> ocanren { add (S O) O O
+                                    | add (S(S O)) (S O) (S O) })
+            (fun q -> q#reify reify);;
+
+
+printf "\n 3 adds 4 equals to what ? \n\n";;
+
+let _ = List.iter (fun x -> print_endline @@ GT.show(ground) x) @@ take @@
+          run one
+            (fun q -> ocanren { add (S(S(S O))) (S(S(S(S O)))) q })
+            (fun q -> q#prj);;
+
+
+printf "\n 3 adds what equals to 7 ? \n\n";;
+
+let _ = List.iter (fun x -> print_endline @@ GT.show(ground) x) @@ take @@
+          run one
+            (fun q -> ocanren { add (S(S(S O))) q (S(S(S(S(S(S(S O))))))) })
+            (fun q -> q#prj);;
+
+
+printf "\n What adds 4 equals to 7 ? \n\n";;
+
+let _ = List.iter (fun x -> print_endline @@ GT.show(ground) x) @@ take @@
+          run one
+            (fun q -> ocanren { add q (S(S(S(S O)))) (S(S(S(S(S(S(S O))))))) })
+            (fun q -> q#prj);;
+
+
+printf "\n What adds what equals to 7 ? \n\n";;
+
+let _ =
+  let printer =
+    fun x,y -> print_endline @@ ("(" ^ GT.show(ground) x ^ " , " ^ GT.show(ground) y ^ ")")
+  in List.iter printer
+     @@ take 
+     @@ run two (fun q r -> ocanren { add q r (S(S(S(S(S(S(S O))))))) })
+          (fun q r -> q#prj, r#prj);;
+
+
+let _ =
+  let ans_no = 10 in
+  printf "\n What adds 4 equals to what ? (Give %d answers) \n\n" ans_no;
+  let printer =
+    fun x,y -> print_endline @@ ("(" ^ GT.show(ground) x ^ " , " ^ GT.show(ground) y ^ ")")
+  in List.iter printer
+     @@ take ~n:ans_no
+     @@ run two (fun q r -> ocanren { add q  (S(S(S(S O)))) r })
+          (fun q r -> q#prj, r#prj);;
+
+
+printf "\n 3 adds what equals to what ? \n\n";;
+
+let _ =
+  let printer =
+    fun x,y -> print_endline @@ ("(" ^ GT.show(logic) x ^ " , " ^ GT.show(logic) y ^ ")")
+  in List.iter printer
+     @@ take 
+     @@ run two (fun q r -> ocanren { add (S(S(S O))) q r })
+          (fun q r -> q#reify reify, r#reify reify);;
+
+
+let _ =
+  let ans_no = 10 in
+  printf "\n What adds what equals to what ? (Give %d answers) \n\n" ans_no;
+  let printer =
+    fun x,y,z -> print_endline @@
+                   ("(" ^ GT.show(ground) x ^ " , "
+                    ^ GT.show(logic) y ^ " , "
+                    ^ GT.show(logic) z ^ ")")
+  in List.iter printer
+     @@ take ~n:ans_no
+     @@ run three (fun a b c -> ocanren { add a b c })
+          (fun a b c -> a#prj, b#reify reify, c#reify reify);;
 
