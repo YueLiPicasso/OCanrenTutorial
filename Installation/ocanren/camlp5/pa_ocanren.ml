457304185fd600db7717ae0565b739ec043ce91a
YueLiPicasso <yue.li.picasso@qq.com>
2020-12-17 15:38:48 +0800

    editing decorate_type 2

--- Installation/ocanren/camlp5/pa_ocanren.ml
+++ Installation/ocanren/camlp5/pa_ocanren.ml
@@ -159,7 +159,7 @@ let rec decorate_type ctyp =
   | <:ctyp< ocanren  $t$ >>  -> t
   | <:ctyp< list $y$ >>      -> <:ctyp< OCanren.Std.List.logic $decorate_type y$ >>                               
   | <:ctyp< option $y$ >>    -> <:ctyp< OCanren.Std.Option.logic $decorate_type y$ >>                               
-  | <:ctyp< $x$ $y$ >>       -> let t = <:ctyp< $decorate_type x$ $decorate_type y$ >> in <:ctyp< OCanren.logic $t$ >>
+  | <:ctyp< $x$ $y$ >>       -> <:ctyp< $decorate_type x$ $decorate_type y$ >> 
   | <:ctyp< $p$ . $t$ >>     -> <:ctyp< OCanren.logic $ctyp$ >>
   | <:ctyp< ( $list:ts$ ) >> -> fold_right1 (fun t1 t2 -> <:ctyp< OCanren.Std.Pair.logic $t1$ $t2$ >> ) @@ List.map decorate_type ts
   | _                        -> ctyp
